7b98b15257a872db8f684e65a58db02cfc694fbc
Adrian140604 <adriansivianes88@gmail.com>
2025-02-20 10:54:48 +0100

    avances

--- proyecto-laravel/resources/views/productos.blade.php
+++ proyecto-laravel/resources/views/productos.blade.php
@@ -8,20 +8,19 @@
                 <img src="Funko.png" alt="Funko Pop" class="w-full h-auto max-h-56 object-contain rounded-md">
                 <h3 class="mt-4 text-2xl font-semibold">Coleccionables</h3>
                 <p class="mt-2 text-gray-400">Figuras coleccionables de tus personajes favoritos.</p>
-                <button class="mt-4 bg-red-600 text-white px-6 py-2 rounded-lg hover:bg-red-700">Comprar</button>
+                <a href="/productos" class="mt-6 inline-block bg-red-600 text-white px-8 py-4 rounded-lg text-lg font-bold hover:bg-red-700 transition">Comprar</a>
             </div>
             <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
                 <img src="EldenRing.jpg" alt="Videojuegos" class="w-full h-auto max-h-56 object-contain rounded-md">
                 <h3 class="mt-4 text-2xl font-semibold">Videojuegos</h3>
                 <p class="mt-2 text-gray-400">Explora una amplia selección de títulos para todas las plataformas.</p>
-                <button class="mt-4 bg-red-600 text-white px-6 py-2 rounded-lg hover:bg-red-700">Comprar</button>
+                <a href="/productos" class="mt-6 inline-block bg-red-600 text-white px-8 py-4 rounded-lg text-lg font-bold hover:bg-red-700 transition">Comprar</a>
             </div>
             <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
                 <img src="poster.jpg" alt="Pósteres" class="w-full h-auto max-h-56 object-contain rounded-md">
                 <h3 class="mt-4 text-2xl font-semibold">Pósteres</h3>
                 <p class="mt-2 text-gray-400">Decora tu espacio con imágenes épicas y únicas.</p>
-                <button class="mt-4 bg-red-600 text-white px-6 py-2 rounded-lg hover:bg-red-700">Comprar</button>
-            </div>
+                <a href="/productos" class="mt-6 inline-block bg-red-600 text-white px-8 py-4 rounded-lg text-lg font-bold hover:bg-red-700 transition">Comprar</a>            </div>
         </div>
     </section>
 
